a5d3d29aaaef584179a643e4059971ab4cfe23c2
Shami_Mourya <shamimourya9852@gmail.com>
2024-05-18 16:28:34 +0530

    header border

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,18 +1,33 @@
 import { NavLink } from "react-router-dom";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import StorageIcon from "@mui/icons-material/Storage";
 
-import GitHubIcon from '@mui/icons-material/GitHub';
-import StorageIcon from '@mui/icons-material/Storage';
-export default function Header(){
-  return <>
-    <div className="logo">School</div>
-    <nav>
-      <NavLink to='/'>Home</NavLink>
-      <NavLink to='/teachers'>Teachers</NavLink>
-      <NavLink to='/students'>Students</NavLink>
-      <NavLink to='/class'>Class view</NavLink>
-      <NavLink to='https://github.com/ShamiKantMourya/School-Management-System' target="_blank"><GitHubIcon/></NavLink>
-      <NavLink to='https://replit.com/@shamiMourya/SchoolManagementBackend'target="_blank"><StorageIcon/></NavLink>
+import mbns from "../assets/mbns.jpg";
 
-    </nav>
-  </>
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src={mbns} alt="school" />
+      </div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/teachers">Teachers</NavLink>
+        <NavLink to="/students">Students</NavLink>
+        <NavLink to="/class">Class view</NavLink>
+        <NavLink
+          to="https://github.com/ShamiKantMourya/School-Management-System"
+          target="_blank"
+        >
+          <GitHubIcon />
+        </NavLink>
+        <NavLink
+          to="https://replit.com/@shamiMourya/SchoolManagementBackend"
+          target="_blank"
+        >
+          <StorageIcon  style={{color: "orangered"}}/>
+        </NavLink>
+      </nav>
+    </div>
+  );
 }
